c6d6f8eff31cd21e5318f23b43ba2c82865816ce
ganicola78-cloud <ganicola78@gmail.com>
2026-06-27 10:55:17 +0200

    Update NavListenerService.java

--- wear/src/main/java/com/example/ciaobeeline/wear/NavListenerService.java
+++ wear/src/main/java/com/example/ciaobeeline/wear/NavListenerService.java
@@ -1,14 +1,23 @@
 package com.example.ciaobeeline.wear;
 
+import android.content.Intent;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 import java.nio.charset.StandardCharsets;
 
 public class NavListenerService extends WearableListenerService {
-    @Override public void onMessageReceived(MessageEvent event) {
+    public static final String ACTION_NAV_UPDATE = "com.example.ciaobeeline.NAV_UPDATE";
+    public static final String EXTRA_JSON = "json";
+
+    @Override
+    public void onMessageReceived(MessageEvent event) {
         if ("/nav_update".equals(event.getPath())) {
             String msg = new String(event.getData(), StandardCharsets.UTF_8);
-            MainActivity.updateFromJson(msg);
+
+            Intent intent = new Intent(ACTION_NAV_UPDATE);
+            intent.setPackage(getPackageName());
+            intent.putExtra(EXTRA_JSON, msg);
+            sendBroadcast(intent);
         }
     }
 }
